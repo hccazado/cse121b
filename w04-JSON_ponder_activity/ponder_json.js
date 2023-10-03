ccefb3e8bd777e55b83e8548eef0fd5737a2e5e1
hccazado <heitorcazado@gmail.com>
2023-10-03 11:12:12 -0300

    w4-ponder_activity - JSON - completed

---
+++ w04-JSON_ponder_activity/ponder_json.js
@@ -0,0 +1,44 @@
+const superHeroes = {
+    "squadName": "Super hero squad",
+    "homeTown": "Metro City",
+    "formed": 2016,
+    "secretBase": "Super tower",
+    "active": true,
+    "members": [
+      {
+        "name": "Molecule Man",
+        "age": 29,
+        "secretIdentity": "Dan Jukes",
+        "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+      },
+      {
+        "name": "Madame Uppercut",
+        "age": 39,
+        "secretIdentity": "Jane Wilson",
+        "powers": [
+          "Million ton punch",
+          "Damage resistance",
+          "Superhuman reflexes"
+        ]
+      },
+      {
+        "name": "Eternal Flame",
+        "age": 1000000,
+        "secretIdentity": "Unknown",
+        "powers": [
+          "Immortality",
+          "Heat Immunity",
+          "Inferno",
+          "Teleportation",
+          "Inter-dimensional travel"
+        ]
+      }
+    ]
+}
+
+
+document.querySelector("#title").innerHTML = "Ponder Activity - JSON";
+
+document.querySelector("#output").innerHTML = `Super Heroe: <br>
+                                                ${superHeroes.members[1].name} <br>
+                                                Power[2]: ${superHeroes.members[1].powers[2]}`;
